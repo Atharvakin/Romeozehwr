ea1844205cfdfff4ddc1df821e4fbdd52fb5a83b
Atharvakin <158204882+Atharvakin@users.noreply.github.com>
2024-01-30 23:02:37 +0530

    Update __init__.py

--- Romeo/__init__.py
+++ Romeo/__init__.py
@@ -8,18 +8,18 @@ SUDO_USERS.append(OWNER_ID)
 app = Client(
     name="app",
     api_id=API_ID,
-    api_hash=API_HASH,
+    api_hash=API_HASH, 
     bot_token=BOT_TOKEN,
     plugins=dict(root="Romeo/plugins"),
     in_memory=True,
 )
 
 client = Client(
-    name="rj",
-    api_id=API_ID,
-    api_hash=API_HASH,
+    name="demon",
+    api_id=API_ID,22652483
+    api_hash=API_HASH,3046c28ec83815dcee7f2a6e3a82127b
     bot_token=BOT_TOKEN,
-    session_string=STRING_SESSION,
+    session_string=STRING_SESSION,BQBociWuKpiIGHB1zDdf0JKEdQKqlmI95Jlpx3ECpRTMDLG63KfIPhpms9odbDsoKN8f1gguKqu4Lrd0BlBCzuTjCdu6DzDR1S0GlEhFnZjD_0Vyf275hc3eZ6IP_riqaY63NnNRPvPzEP7Qhl4HbjKv1_NHD-RSY_X8MA6Fy5zkwjkIXK5dvCjQjvPhlqt4QAkmeDTW0JmBl4RC4d-PpDf9HLuW-fDkrY0yickOAIv2Xvcntc9-zdmkihN4blolzBZ3SKVI5CG-hdqatI9mjEmZNFaBsJVqG2Y1rk0EO73qYE51Sjmc0UdFc43R2J0Vxi5dXunDICWtzH6I2u6PJKVFAAAAAZSqtywA
     plugins=dict(root="Romeo/plugins")
 )
 
